7604e46777fefcecccffad8299abfa3376b52511
NikolaMX <nikola.mixailov@abv.bg>
2025-05-23 12:43:41 +0300

    initial commit

---
+++ ui/mods/2v2_tourney/maplist.js
@@ -0,0 +1,17 @@
+cShareSystems.load_pas("Factions 2v2 NS", [
+
+
+"insanateria 2v2 ns.pas",
+"abstract 2v2 ns.pas",
+"cobble_pass 2v2 ns.pas",
+"glacier 2v2 ns.pas",
+"monsoon 2v2 ns.pas",
+"PAX Frontiers 2v2 ns.pas",
+"t-kure atoll 2v2 ns.pas",
+"t-the island 2v2 ns.pas",
+"tandem 2v2 ns.pas",
+"thief's pass 2v2 ns.pas",
+"tropicanhoneydonut 2v2 ns.pas",
+"verity's canal 2v2 ns.pas"
+]);
+
