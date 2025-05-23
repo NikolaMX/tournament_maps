e795cf4ea0b8478de9ed25eafb22d857595a7c6b
NikolaMX <nikola.mixailov@abv.bg>
2025-05-23 23:13:17 +0300

    Update maplist.js

--- ui/mods/2v2_tourney/maplist.js
+++ ui/mods/2v2_tourney/maplist.js
@@ -1,17 +1,14 @@
 cShareSystems.load_pas("Factions 2v2 NS", [
-
-
-"insanateria 2v2 ns.pas",
-"abstract 2v2 ns.pas",
-"cobble_pass 2v2 ns.pas",
-"glacier 2v2 ns.pas",
-"monsoon 2v2 ns.pas",
-"PAX Frontiers 2v2 ns.pas",
-"t-kure atoll 2v2 ns.pas",
-"t-the island 2v2 ns.pas",
-"tandem 2v2 ns.pas",
-"thief's pass 2v2 ns.pas",
-"tropicanhoneydonut 2v2 ns.pas",
-"verity's canal 2v2 ns.pas"
+"coui://ui/mods/2v2_tourney/systems/insanateria 2v2 ns.pas",
+"coui://ui/mods/2v2_tourney/systems/abstract 2v2 ns.pas",
+"coui://ui/mods/2v2_tourney/systems/cobble_pass 2v2 ns.pas",
+"coui://ui/mods/2v2_tourney/systems/glacier 2v2 ns.pas",
+"coui://ui/mods/2v2_tourney/systems/monsoon 2v2 ns.pas",
+"coui://ui/mods/2v2_tourney/systems/PAX Frontiers 2v2 ns.pas",
+"coui://ui/mods/2v2_tourney/systems/t-kure atoll 2v2 ns.pas",
+"coui://ui/mods/2v2_tourney/systems/t-the island 2v2 ns.pas",
+"coui://ui/mods/2v2_tourney/systems/tandem 2v2 ns.pas",
+"coui://ui/mods/2v2_tourney/systems/thief's pass 2v2 ns.pas",
+"coui://ui/mods/2v2_tourney/systems/tropicanhoneydonut 2v2 ns.pas",
+"coui://ui/mods/2v2_tourney/systems/verity's canal 2v2 ns.pas"
 ]);
-
